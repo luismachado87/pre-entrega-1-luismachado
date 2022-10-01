8e252c3f67424a8776aa533bbbea355bc62dda00
luis machado <lmachado8787@gmail.com>
2022-10-01 14:00:30 -0300

    cambiosviernes

--- src/data.js
+++ src/data.js
@@ -1,6 +1,12 @@
 import img1 from './img/img1.jpg';
 import img2 from './img/img2.webp';
 import img3 from './img/img3.webp';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+
 
 const data = {
     productData:[
@@ -26,6 +32,46 @@ const data = {
             title: 'moras',
             desc:'',
             price: 200,
+    
+        },
+        {
+            id: 4,
+            img: img4,
+            title: 'cerezas',
+            desc:'',
+            price: 150,
+    
+        },
+        {
+            id: 5,
+            img: img5,
+            title: 'peras',
+            desc:'',
+            price: 400,
+    
+        },
+        {
+            id: 6,
+            img: img6,
+            title: 'manzanas',
+            desc:'',
+            price: 250,
+    
+        },
+        {
+            id: 7,
+            img: img7,
+            title: 'coco',
+            desc:'',
+            price: 350,
+    
+        },
+        {
+            id: 8,
+            img: img8,
+            title: 'mango',
+            desc:'',
+            price: 300,
     
         }
     ],
